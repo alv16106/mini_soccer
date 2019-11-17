4e4caadb829e29e283226d63f1657c721c7e5943
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2019-11-17 16:34:23 -0600

    add shoot and collision

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let field, ball, robo
-const goal = [1450, 485, 485 + 130]
+const goal = {x:1450, y:550, r:35}
+const error_angle = 10
 
 const getRaw = (ball, robot) =>{
   d = Math.pow(ball.x - robot.x, 2) + Math.pow(ball.y - robot.y, 2)
@@ -20,31 +21,60 @@ function setup() {
 
 function draw() {
   image(field, 0, 0)
-  // image(ball, 350, 350, 40, 40);
+
   robo.update()
   ball.update()
   robo.show()
   ball.show()
+
+  // Collision detection
+  d = dist(robo.x, robo.y, ball.x, ball.y)
+  if (d < robo.r + ball.r) {
+    ball.shoot()
+  }
+}
+
+// For testing
+function keyPressed(){
+  if(keyCode == 87){
+    robo.xspeed = 0
+    robo.yspeed = 1
+  } else if(keyCode == 83){
+    robo.xspeed = 0
+    robo.yspeed = -1
+  } else if(keyCode == 68){
+    robo.xspeed = 1
+    robo.yspeed = 0
+  } else if(keyCode == 65){
+    robo.xspeed = -1
+    robo.yspeed = 0
+  }
 }
 
 function Robot() {
   this.x = 800
   this.y = 500
+  this.r = 40
   this.xspeed = 0
   this.yspeed = 0
 
   this.update = (ballx, bally) => {
     // Update code with fuzzy logic
+    this.x += this.xspeed
+    this.y += this.yspeed
   }
 
   this.show = () => {
-    square(this.x, this.y, 55, 20);
+    rectMode(RADIUS)
+    square(this.x, this.y, this.r, 20);
   }
 }
 
 function Ball() {
   this.x = 350
   this.y = 350
+  this.r = 20
+  this.smultiplier = 3
   this.xspeed = 0
   this.yspeed = 0
 
@@ -54,6 +84,15 @@ function Ball() {
   }
 
   this.show = () => {
-    ellipse(this.x, this.y, 40, 40)
+    circle(this.x, this.y, this.r*2)
+  }
+
+  this.shoot = () => {
+    let to_center = Math.atan2(goal.y - this.y, goal.x - this.x);
+    to_center = to_center * 180 / Math.PI;
+    let shoot_angle = to_center + Math.floor(Math.random() * error_angle * 2) - error_angle;
+    shoot_angle = shoot_angle * Math.PI / 180
+    this.yspeed = sin(shoot_angle) * this.smultiplier
+    this.xspeed = cos(shoot_angle) * this.smultiplier
   }
 }
